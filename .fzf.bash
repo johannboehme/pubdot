d2d2fd3b64e3403e1c22be18dca779a00feb19d9
johannboehme <devien@PC2014-4056.asc.de>
2021-01-23 17:26:15 +0100

    changed fzf shortcut location

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/lorddevien/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/lorddevien/.fzf/bin"
+if [[ ! "$PATH" == */root/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/root/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/lorddevien/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/root/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/lorddevien/.fzf/shell/key-bindings.bash"
+source "/root/.fzf/shell/key-bindings.bash"
